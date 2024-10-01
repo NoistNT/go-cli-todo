1508b6dcea522f6c2d80d3a934caf99d2d1f7d50
NoistNT <arielgnr23@gmail.com>
2024-10-01 14:34:52 -0300

    Add switch cases to handle commands to main.go

---
+++ cmd/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/NoistNT/go-cli-todo/internal/todos"
+	helpers "github.com/NoistNT/go-cli-todo/pkg"
+)
+
+func main() {
+	const (
+		titleMsg      = "What should we write in the todo?: "
+		errorIDMsg    = "Error reading ID"
+		errorTitleMsg = "Error reading title"
+	)
+
+	todos.Init()
+	helpers.PrintCommands()
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		command, err := helpers.GetInput(reader, "[cmd]: ")
+		helpers.ErrorHandler(err)
+
+		switch command {
+		case "add":
+			title, err := helpers.GetInput(reader, titleMsg)
+			helpers.ErrorHandler(err)
+			todos.Add(title)
+
+		case "list":
+			todos.List()
+
+		case "update":
+			id, err := helpers.GetInputInt(reader, "Enter todo ID to update: ")
+			helpers.ErrorHandler(err)
+			title, err := helpers.GetInput(reader, titleMsg)
+			helpers.ErrorHandler(err)
+			todos.Update(id, title)
+
+		case "done":
+			id, err := helpers.GetInputInt(reader, "Enter todo ID to mark as done: ")
+			helpers.ErrorHandler(err)
+			todos.Done(id)
+
+		case "remove":
+			id, err := helpers.GetInputInt(reader, "Enter todo ID to remove: ")
+			helpers.ErrorHandler(err)
+			todos.Remove(id)
+
+		case "exit", "quit":
+			fmt.Println("Program ended successfully")
+			os.Exit(0)
+
+		default:
+			fmt.Println("Invalid command!")
+			helpers.PrintCommands()
+		}
+	}
+}
